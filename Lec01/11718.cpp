9c394d9a2be1eea21788a61568853123aa998081
Daria-SubinPark <lmhapy25@khu.ac.kr>
2020-02-17 09:56:38 +0900

    Lec01

---
+++ Lec01/11718.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    while(true)
+    {
+        getline(cin, str);
+        if(str == "")
+            break;
+        cout << str << endl;
+    }
+}
